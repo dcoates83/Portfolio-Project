6089d48a06367d7615d54a6ad7af061033b8acfd
Devon Coates <63134707+dcoates83@users.noreply.github.com>
2021-07-08 16:02:11 -0600

    updated main page css for cleaner look

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -1,13 +1,44 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/Layout"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-const ProjectsPage = () => {
+const ProjectsPage = ({
+  data: {
+    allStrapiProjects: { nodes: projects },
+  },
+}) => {
   return (
-    <>
-      <h2>projects page</h2>
-    </>
+    <Layout>
+      <section className="projects-page">
+        <Projects projects={projects} title="All Projects" />
+      </section>
+    </Layout>
   )
 }
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        description
+        id
+        title
+        url
+        github
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
