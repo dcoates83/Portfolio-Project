d649b26bf90e5f825844f20ff60818409b3c86a1
Devon Coates <63134707+dcoates83@users.noreply.github.com>
2021-07-08 11:54:27 -0600

    added strapi backend to same folder

---
+++ portfolio-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '70e0af5b-d57c-435f-aaf0-0967410bdd7a'
+};
--- starter-project-gatsby-v3-strapi-portfolio-main/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import socialLinks from "../constants/social_links"
-const Footer = () => {
-  return <h2>footer component</h2>
-}
-
-export default Footer
